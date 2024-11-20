48393eb27ab2d889a9ed839f208377db58736d03
EOJIN KIM <kimeojin1995@gmail.com>
2024-11-20 20:26:31 +0900

    slide 오른쪽 이동 일부 구현

--- src/app/_components/homeEggProductSlide/index.tsx
+++ src/app/_components/homeEggProductSlide/index.tsx
@@ -1,6 +1,53 @@
 "use client";
 
+import { log } from "console";
+import { useEffect, useRef, useState } from "react";
+
+const useViewport = () => {
+  const [viewport, setViewport] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    // 클라이언트 사이드에서만 실행
+    const handleResize = () => {
+      setViewport({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    // 초기 사이즈 설정
+    handleResize();
+
+    // 리사이즈 이벤트 리스너 추가
+    window.addEventListener("resize", handleResize);
+
+    // 컴포넌트 언마운트 시 리스너 제거
+    return () => {
+      console.log("unmount");
+
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // 빈 배열을 넣어 컴포넌트가 마운트된 후 한 번만 실행되도록
+
+  return viewport;
+};
+
 const HomeEggProductSlide: React.FC = () => {
+  const rightButtonEle = useRef<HTMLInputElement>(null);
+  const [currentSlideNum, setCurrentSlideNum] = useState(0);
+  const { width, height } = useViewport();
+  const calcSlidePx = `${width / 2 - 2925 - 650 * currentSlideNum}px`;
+
+  const RightButtonClick = () => {
+    setCurrentSlideNum(currentSlideNum + 1);
+  };
+
+  useEffect(() => {
+    return;
+  });
   return (
     <section className="h-[950px]">
       <div className="relative overflow-hidden">
@@ -12,7 +59,23 @@ const HomeEggProductSlide: React.FC = () => {
           src="/images/slideBottom.svg"
           className="absolute bottom-[-1px] h-[97px] z-10 w-full"
         />
-        <div className="flex translate-x-[calc(50vw-2925px)]">
+        <div className="absolute flex top-32 justify-center items-center z-30 w-[120px] h-[120px] border-[1px] border-solid border-[#fff] rounded-full">
+          <img src="/images/cursor.svg" className="scale-[-1]" />
+        </div>
+        <div
+          onClick={RightButtonClick}
+          ref={rightButtonEle}
+          className="absolute right-0 top-32 hover:cursor-pointer flex justify-center items-center z-30 w-[120px] h-[120px] border-[1px] border-solid border-[#fff] rounded-full"
+        >
+          <img src="/images/cursor.svg" />
+        </div>
+
+        <div
+          className={`flex`}
+          style={{
+            transform: `translateX(${calcSlidePx})`,
+          }}
+        >
           <div className="w-[650px]">
             <img
               src="/images/gradient-bg-blue.png"
